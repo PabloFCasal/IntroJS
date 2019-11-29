0b53e2734e6b96443aa624ff66337b824f26d4ba
Pablo Fernández <pablofcasal@gmail.com>
2019-11-29 20:42:41 +0100

    Main rules and first filter

--- Kata_2/main.js
+++ Kata_2/main.js
@@ -39,4 +39,37 @@ Sólo se contemplan números entre el 1 y el 3999
 
 */
 
+function arabToRoman(n) {
+
+    let result;
+
+    let basicRules = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    if( n < 1 || n > 3999){
+
+        console.log('Please use only numbers between 1 and 3999');
+    }
+
+
+
+
+
+
+
+}
+
 
